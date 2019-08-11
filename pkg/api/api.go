c2836473f2df23304018d0a00bbc7a754184adf0
Zach P <zachpuckett@gmail.com>
2019-08-11 15:22:57 -0700

    wip

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -8,26 +8,17 @@ import (
 
 type App struct {
 	V1Handler *V1Handler
-	//FileHandler *http.Handler
+
 
 }
 
 func (h *App) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	var head string
 	head, req.URL.Path = urlpath.ShiftPath(req.URL.Path)
-	fmt.Fprintln(res,"HEAD=", head)
-
 	if head == "v1" {
+		fmt.Println("v1 path: ", req.URL.Path)
 		h.V1Handler.ServeHTTP(res, req)
 		return
 	}
 	http.Error(res, "Not Found", http.StatusNotFound)
-}
-
-type V1Handler struct {
-}
-
-func (h *V1Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Not Implemented!")
-	return
 }
---
+++ pkg/api/v1.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"fmt"
+	"github.com/zachpuck/dokuweb-api/pkg/util/urlpath"
+	"net/http"
+)
+
+type V1Handler struct {
+	ImageHandler *ImageHandler
+}
+
+func (h *V1Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	var head string
+	if req.URL.Path != "/" {
+		head, req.URL.Path = urlpath.ShiftPath(req.URL.Path)
+		switch head {
+		case "images":
+			fmt.Println("images path: ", req.URL.Path)
+			h.ImageHandler.ServeHTTP(res, req)
+		default:
+			http.Error(res, "Not Found", http.StatusNotFound)
+		}
+		return
+	}
+	//
+	switch req.Method {
+	case "GET":
+		h.handleGet(res, req)
+	default:
+		http.Error(res, "Only GET are allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *V1Handler) handleGet(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "v1 root")
+	return
+}
+
+type ImageHandler struct {
+
+}
+
+func (h *ImageHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res,"not implemented")
+	return
+}
